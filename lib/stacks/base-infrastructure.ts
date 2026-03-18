ff604f48d72d5881b767af7141c85b4c4b2ca098
nikensonmidi <nikensonmidi@gmail.com>
2026-03-18 13:10:53 -0400

    added infra back https://github.com/Waney93LLC/cdkapps/issues/55

--- lib/stacks/base-infrastructure.ts
+++ lib/stacks/base-infrastructure.ts
@@ -29,16 +29,16 @@ export class BaseInfrastructureStack extends cdk.Stack {
     const config = getWaney93PipelineConfig(this, props.stage);
     
 
-    // new BaseInfrastructureBuilder(this, id, {
-    //   stage: props.stage,
-    //   pipelineName: config.baseInfrastructure.pipeline.name,
-    //   network: config.baseInfrastructure.network,
-    //   exportNames: config.baseInfrastructure.exportNames,
-    // })
-      // .withNetwork()
-      // .withRdsBastion(config.baseInfrastructure.bastion)
-      // .withAppClientSecurityGroup()
-      // .withAuroraDB(config.baseInfrastructure.rds)
-      // .outputs();
+    new BaseInfrastructureBuilder(this, id, {
+      stage: props.stage,
+      pipelineName: config.baseInfrastructure.pipeline.name,
+      network: config.baseInfrastructure.network,
+      exportNames: config.baseInfrastructure.exportNames,
+    })
+      .withNetwork()
+      .withRdsBastion(config.baseInfrastructure.bastion)
+      .withAppClientSecurityGroup()
+      .withAuroraDB(config.baseInfrastructure.rds)
+      .outputs();
   }
 }
